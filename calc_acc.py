9ff5aeb28d6cf3c14bc5e0534b9585920ead545d
avinanakarmi <avinanakarmi5@gmail.com>
2017-12-10 13:28:40 +0545

    3. calculate acceleration

--- calc_acc.py
+++ calc_acc.py
@@ -2,9 +2,9 @@ def acc(v,u,t):
     """ function to calculate acceleration"""
     return ((v-u)/t)
 
-print("we know\n\tv = u + at\n\ta = (v-u)/t")
+print("we know\n\tv = u + at")
 v=25
 u=0
 t=10
 unit="m/s"
-print("for v = {0}{3}, u = {1}{3} and t = {2}s\nwe have,\n\ta = {4}m/s\u00b2".format(v,u,t,unit,acc(v,u,t)))
+print("for v = {0}{3}, u = {1}{3} and t = {2}s\nwe have,\n\t{0} = {1} + {2}a\n\ta = ({0} - {1})/{2}\n\ta = {4}m/s\u00b2".format(v,u,t,unit,acc(v,u,t)))
